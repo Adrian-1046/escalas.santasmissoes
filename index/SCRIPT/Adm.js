01f8ee153f58ec08f0fc8a29de03ab137267eeba
Adrian Bellezzo Medeiros <98426757+Adrian-1046@users.noreply.github.com>
2024-01-08 19:57:36 -0300

    Delete index directory

--- index/SCRIPT/Adm.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const botaoAcesso = document.getElementById('acesso');
-  ///validar usuario para exibir só pra adm
-  botaoAcesso.style.visibility = 'visible';
-});

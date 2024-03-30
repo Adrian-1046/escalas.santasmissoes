69678ddf737d5f8ee20573f29c55668cb9b112f7
Adrian Bellezzo Medeiros <98426757+Adrian-1046@users.noreply.github.com>
2024-03-30 19:15:50 -0300

    Add files via upload

---
+++ banco.js
@@ -0,0 +1,30 @@
+//para integrar banco de dados:
+//npm install --save sequelize
+
+//para utilizar mysql
+//npm install -s mysql2@3.0.0
+
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const banco = new Sequelize(
+  process.env.MSQLDBAS,
+  process.env.MSQLUSER,
+  process.env.MSQLPASS,
+  {
+    quoteIdentifiers: false,
+    host: process.env.MSQLHOST,
+    port: process.env.MSQLPORT,
+     dialect: 'mysql',
+
+  }
+);
+
+banco
+  .authenticate()
+  .then(console.log(`Conectado com sucesso!`))
+  .catch(function (err) {
+    console.log(`Falha ao conectar ao banco de dados: ` + err);
+  });
+
+module.exports = banco;
